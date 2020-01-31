40534970d947e8dbff2c9e9a2803724d232a960f
Richard <richardtran298@gmail.com>
2020-01-30 16:18:52 -0800

    Lab 1 files

--- lab1/src/Arrays.java
+++ lab1/src/Arrays.java
@@ -1,36 +1,39 @@
 
 public class Arrays {
-
-    int largest = 0;
-    int largestIndex;
-    int smallest = Integer.MAX_VALUE;
-    int smallestIndex;
     
-    public int[] swapLargestAndSmallest(int[] a) 
+    public static int[] swapLargestAndSmallest(int[] a) 
     {
+        int largest = 0;
+        int largestIndex = 0;
+        int smallest = Integer.MAX_VALUE;
+        int smallestIndex = 0;
+        int temp;
+        
         if (a.length == 1) 
         {
            return a; 
         }    
-        for (int i = 0; i < a.length; i++)
+        else 
         {
-            if (a[i] > largest)
+            for (int i = 0; i < a.length; i++)
             {
-                largest = a[i];
-                largestIndex = i;
+                if (a[i] > largest)
+                {
+                    largest = a[i];
+                    largestIndex = i;
+                }
+                if (a[i] < smallest)
+                {
+                    smallest = a[i];
+                    smallestIndex = i;
+                }
+                
             }
-            if (a[i] < smallest)
-            {
-                smallest = a[i];
-                smallestIndex = i;
-            }
+            temp = a[largestIndex];
+            a[largestIndex] = a[smallestIndex];
+            a[smallestIndex] = temp;
             
+            return a;
         }
-        int temp = a[largestIndex];
-        a[largestIndex] = a[smallestIndex];
-        a[smallestIndex] = temp;
-        
-        return a;
     }
-    
 }
